015098e7b8b9f2426d62b96d301a58b03baaec4f
Elijah <ekollie324@gmail.com>
2023-12-16 18:56:32 -0500

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import ipdb
+
+class Anagram():
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, input_list):
+        deconstructed_word = sorted([letter for letter in self.word])
+        return [word for word in input_list if sorted([letter for letter in word]) == deconstructed_word]
+
+# ipdb.set_trace()
